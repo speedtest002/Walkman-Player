19bdf15ef26fd4d4333e9ffd76e40bdfcad1af6e
speedtest002 <huyprokute002@gmail.com>
2024-05-24 20:37:10 +0700

    second commit

---
+++ WalkmanPlayer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WalkmanPlayer
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         MainPage = new AppShell();
+      }
+   }
+}
